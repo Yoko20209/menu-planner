f12b897476b530f2e8aa69f02bc74878ac4e4807
Yoko Tsuda <yoko20209@gmail.com>
2021-04-28 16:49:25 +0900

    deleted comments and unused components

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,56 +1,12 @@
 import React, {useState, useEffect, useCallback} from 'react'; 
-import Navbar2 from "./Navbar2"
-
-// function SearchResult(){
 
 
-//     return(
-//         <div>Search Result  
-//         </div>
-//     )
-// }
 function SearchResult() {
-    const [number, setNumber] = useState();
-    const [button, setButton] = useState("false");
-  
-    const makeArray = useCallback(
-      (num) => {
-        return [num, num, num];
-      },
-      [number]
-    );
-    
-    const numberArr = makeArray(number);
+
   
     return (
       <>
-      <div className="Navbar">
-               <input
-          type="text"
-          value={number}
-          onChange={(e) => {
-            setNumber((e.target.value));
-          }}
-        ></input>
-        {numberArr}
-      </div>
-   
-  
-        <button
-          onClick={() => {
-            return setButton("true");
-          }}
-        >
-          true
-        </button>
-        <button
-          onClick={() => {
-            return setButton("false");
-          }}
-        >
-          false
-        </button>
-        {button}
+      Search Result
       </>
     );
   }
